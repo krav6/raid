71921b1142d9939dd9264422a83e8b33e4e9d141
krav6 <patrik.szabo.92@gmail.com>
2018-08-31 17:11:23 +0900

    [feat] Added basic character movement and animation

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -5,20 +5,26 @@ using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour {
 
-    public Camera cam;
+    public float speed;
 
-    public NavMeshAgent agent;
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetMouseButtonDown(0))
-        {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+    private Animator animator;
 
-            if(Physics.Raycast(ray, out hit))
-            {
-                agent.SetDestination(hit.point);
-            }
-        }
-	}
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        bool isWalking = z > 0.0f;
+        bool isWalkingBackwards = z < 0.0f;
+
+        animator.SetBool("isWalking", isWalking);
+        animator.SetBool("isWalkingBackwards", isWalkingBackwards);
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
 }
