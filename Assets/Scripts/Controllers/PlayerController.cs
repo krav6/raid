8f1159e36bd553588d0aada402080a16d3a51c10
krav6 <patrik.szabo.92@gmail.com>
2018-09-02 15:30:13 +0900

    [feat] Added jumping

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -16,29 +16,78 @@ public class PlayerController : MonoBehaviour {
         animator = GetComponent<Animator>();
     }
 
-    void FixedUpdate()
+    bool isJumpingInPlace(AnimatorStateInfo animatorInfo)
     {
-        bool isCrouching = Input.GetKey(KeyCode.Q);
-        //TODO: Consider falling
-        bool isStanding = !isCrouching;
+        return Animator.StringToHash("Base Layer.Jump.Jumping in place") == animatorInfo.fullPathHash;
+    }
+
+    bool isJumpingForward(AnimatorStateInfo animatorInfo)
+    {
+        return Animator.StringToHash("Base Layer.Jump.Forward Jump") == animatorInfo.fullPathHash;
+    }
+
+    bool isJumpingBackward(AnimatorStateInfo animatorInfo)
+    {
+        return Animator.StringToHash("Base Layer.Jump.Backward Jump") == animatorInfo.fullPathHash;
+    }
+
+    bool isJumping(AnimatorStateInfo animatorInfo)
+    {
+        return isJumpingInPlace(animatorInfo) || isJumpingForward(animatorInfo) || isJumpingBackward(animatorInfo);
+    }
+
+    void handleJumping(bool isCrouching, AnimatorStateInfo animatorInfo)
+    {
+        bool isJumping = this.isJumping(animatorInfo);
+        if(isJumping)
+        {
+            animator.ResetTrigger("Jump");
+            return;
+        }
+
+        if (!isJumping && !isCrouching && Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Jump");
+        }
+    }
+
+    void handleVerticalMovement(bool isCrouching, AnimatorStateInfo animatorInfo)
+    {
+        if(isJumpingInPlace(animatorInfo))
+        {
+            return;
+        }
 
         float movementSpeed = isCrouching ? crawlSpeed : walkSpeed;
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
 
-        bool isMovingForward = z > 0.0f;
-        bool isMovingBackward = z < 0.0f;
-        bool isTurningLeft = x < 0.0f;
-        bool isTurningRight = x > 0.0f;
+        animator.SetBool("isMovingForward", z > 0.0f);
+        animator.SetBool("isMovingBackward", z < 0.0f);
+        transform.Translate(0, 0, z);
+    }
 
-        animator.SetBool("isMovingForward", isMovingForward);
-        animator.SetBool("isMovingBackward", isMovingBackward);
-        animator.SetBool("isTurningLeft", isTurningLeft);
-        animator.SetBool("isTurningRight", isTurningRight);
-        animator.SetBool("isCrouching", isCrouching);
-        animator.SetBool("isStanding", isStanding);
+    void handleHorizontalMovement()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+
+        animator.SetBool("isTurningLeft", x < 0.0f);
+        animator.SetBool("isTurningRight", x > 0.0f);
 
         transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+    }
+
+    void FixedUpdate()
+    {
+        var animatorInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        bool isCrouching = Input.GetKey(KeyCode.Q);
+        animator.SetBool("isCrouching", isCrouching);
+
+        handleJumping(isCrouching, animatorInfo);
+        handleVerticalMovement(isCrouching, animatorInfo);
+        handleHorizontalMovement();
+
+        bool isStanding = !isCrouching && !isJumping(animatorInfo);
+        animator.SetBool("isStanding", isStanding);
     }
 }
