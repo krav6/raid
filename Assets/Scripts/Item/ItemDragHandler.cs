587fd92645767fe2beec2fc066d4715688fb0278
krav6 <patrik.szabo.92@gmail.com>
2018-09-14 12:09:23 +0900

    [feat] Added inventory item drag

---
+++ Assets/Scripts/Item/ItemDragHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private bool isDragging = false;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        isDragging = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        isDragging = false;
+        transform.localPosition = Vector3.zero;
+    }
+
+    void Update()
+    {
+        if(isDragging)
+        {
+            transform.position = Input.mousePosition;
+        }
+    }
+}
